51fff19456bd398eaaac2d57eb58c27b49e0e45d
Leograzioli <leograzioli@gmail.com>
2022-10-11 13:44:50 +0200

    esercizio mail fatto

---
+++ mail/js/script.js
@@ -0,0 +1,17 @@
+const userEmailList = ['mario@gmail.com', 'luigi@gmail.com', 'yoshi@gmail.com', 'toad@gmail.com']
+const userEmailInput = prompt("per accedere inserisce la tua mail:")
+let isFound = false;
+
+for (let i = 0; i < userEmailList.length; i++) {
+    const userEmail = userEmailList[i];
+     
+    if(userEmailInput === userEmail) {
+        isFound = true;
+    }
+}
+
+if (isFound) {
+    alert("welcome");
+} else {
+    alert("wrong email"); 
+};
